8f9dc9f6dcbd03e5c6663dd7c61d950b706b882c
Dalmo Silva Mendes <65993036+DalmoMendes@users.noreply.github.com>
2021-08-11 01:26:27 -0300

    Update Front-end-supermercado
    Adicionado arquivos do projeto Fornt-end-Supermercado. HTML e CSS.

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+ <!--
+    CEPROIRR - Centro Profissional de Informática.
+    Modelo Criado por DALMO SILVA MENDES
+    Site: ceproirr.com.br
+    Email coorporativo: ceproirr@ceproirr.com.br
+    Email da Ceproirr: ceproirr@gmail.com
+    Email Pessoal: dalmosilvamendes@gmail.com
+    Tel: (95) 99170-7450
+
+    Fazer Doação
+    Banco do Brasil
+    Agência: 0250-X
+    Conta: 87.489-2
+--> 
+ */
